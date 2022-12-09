3709cf7f3f3213d20f1188a0d5b6ba43bc59e36b
Arqam <Arqam.rafay@sixlogs.com>
2022-12-09 11:10:54 +0500

    add sql challenge

--- sql/queries.sql
+++ sql/queries.sql
@@ -16,3 +16,13 @@ count(submissions.submission_id) > 1
 order by 
 count(submissions.submission_id ) DESC,
 hackers.hacker_id ASC`
+
+<!-- Challenges Find hackerrank  name and Challenges cout -> 
+`SELECT H.hacker_id, H.name, COUNT(C.challenge_id) as no_of_challenges
+FROM Hackers H
+JOIN Challenges C ON H.hacker_id = C.hacker_id
+GROUP BY H.hacker_id, H.name
+HAVING no_of_challenges = (SELECT count(challenge_id) AS max_count FROM Challenges GROUP BY hacker_id ORDER BY max_count DESC LIMIT 1)
+OR no_of_challenges NOT IN (SELECT t.cnt FROM (SELECT count(challenge_id) AS cnt FROM Challenges GROUP BY hacker_id) t GROUP BY t.cnt HAVING COUNT(t.cnt) != 1)
+ORDER BY no_of_challenges DESC, H.hacker_id ASC;
+`
